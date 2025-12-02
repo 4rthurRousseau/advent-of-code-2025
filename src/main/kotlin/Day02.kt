1d56aa6c8319cbf53ae599cb0a690e136e153d03
Arthur Rousseau <arthur.rousseau2@gmail.com>
2025-12-03 00:01:50 +0100

    feat: added day 2 part 2 solution

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,10 +1,7 @@
 class Day02 : Day(2) {
 
     override fun part1(input: List<String>): Any {
-        val ranges = input.first().split(",").map {
-            val (from, to) = it.split("-")
-            Pair(from.toLong(), to.toLong())
-        }
+        val ranges = input.toRanges()
 
         val sum = ranges.sumOf {
             (it.first..it.second).sumOf {
@@ -32,7 +29,40 @@ class Day02 : Day(2) {
     }
 
     override fun part2(input: List<String>): Any {
-        return 0
+        val ranges = input.toRanges()
+        var sum = 0L
+
+        ranges.forEach {
+            (it.first..it.second).forEach outer@{ value ->
+                val text = "$value"
+
+                val limit = text.length / 2
+                limit.downTo(1).filter { text.length % it == 0 }.forEach {
+                    val pattern = text.take(it)
+                    val chunkSize = pattern.length
+                    val chunks = text.length / chunkSize
+
+                    val invalid = (0..<chunks).all { i ->
+                        val value = text.substring(chunkSize * i, chunkSize * (i + 1))
+                        value == pattern
+                    }
+
+                    if (invalid) {
+                        sum += value
+                        return@outer
+                    }
+                }
+            }
+        }
+
+        return sum
+    }
+
+    private fun List<String>.toRanges(): List<Pair<Long, Long>> {
+        return first().split(",").map {
+            val (from, to) = it.split("-")
+            Pair(from.toLong(), to.toLong())
+        }
     }
 }
 
