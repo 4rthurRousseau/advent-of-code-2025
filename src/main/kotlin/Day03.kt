a2e9d9953d27a891378a2b66e879383c48103c34
Arthur Rousseau <arthur.rousseau2@gmail.com>
2025-12-04 17:48:34 +0100

    feat: added day 3 part 1 solution

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,36 @@
+class Day03 : Day(3) {
+
+    override fun part1(input: List<String>): Any {
+        val batteries = input.toBatteries()
+
+        val sum = batteries.sumOf {
+            val x = it.dropLast(1).max()
+            val ix = it.indexOf(x)
+
+            val y = it.filterIndexed { index, _ ->
+                index > ix
+            }.max()
+
+            val best = x * 10 + y
+            println(best)
+
+            best
+        }
+
+        return sum
+    }
+
+    override fun part2(input: List<String>): Any {
+        return 0
+    }
+
+    private fun List<String>.toBatteries(): List<List<Int>> {
+        return this.map {
+            it.map { it.digitToInt() }
+        }
+    }
+}
+
+fun main() {
+    Day03().solve()
+}
